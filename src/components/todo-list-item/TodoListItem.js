3cb81ba1ed19ad01eaf13a1683a16da2d8b97565
Cleber Miranda Barbosa <cleber@gmail.com>
2019-12-13 21:49:01 -0300

    adding remove button

--- src/components/todo-list-item/TodoListItem.js
+++ src/components/todo-list-item/TodoListItem.js
@@ -1,10 +1,15 @@
 import React from "react"
-
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import "./TodoListItem.css"
 
 function TodoListItem(props) {
     return (
-        <li className="list-group-item" id={props.index}>{props.item}
+        <li className="list-group-item " id={props.index}>{props.item}
+        <FontAwesomeIcon className="remove" icon={faTimes}/>
         </li>
+   
     )
 }
 
